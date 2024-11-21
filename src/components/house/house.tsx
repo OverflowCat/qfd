4d5db948d83e6493980e23f98917a59d380d3357
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2024-11-21 14:08:18 +0800

    bind

--- src/components/house/house.tsx
+++ src/components/house/house.tsx
@@ -1,135 +1,236 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useComputed$ } from "@builder.io/qwik";
 import "./house.css";
-interface ItemProps {
+interface Data {
   技术手段: string[];
   用户需求: string[];
+  用户需求权重: number[];
   竞争者评价分制: number;
+  竞争者评价: {
+    [key: string]: boolean;
+  }[][];
+  技术相关性: number[][];
+  程度符号: Array<string | false>;
+  关系矩阵: Array<Array<number>>;
 }
 
-export const House = component$((props: ItemProps) => {
+const Rater = component$(
+  (props: {
+    data: {
+      [key: string]: boolean;
+    };
+    _key: string;
+    handler: any;
+  }) => {
+    const { data, _key, handler } = props;
+    return (
+      <td key={_key}>
+        {Object.entries(data).map(([firm, value], i) => (
+          <>
+            <input
+              key={"rater-" + _key + "-" + i + "-" + firm}
+              id={"c-" + _key + "-" + i + "-" + firm}
+              type="checkbox"
+              name={firm}
+              checked={value}
+              onChange$={() => handler(firm, !value)}
+            ></input>
+            <label
+              for={"c-" + _key + "-" + i + "-" + firm}
+              id={"label-" + _key + "-" + i + "-" + firm}
+            >
+              {firm}
+            </label>
+          </>
+        ))}
+      </td>
+    );
+  }
+);
+
+export const House = component$((props: { data: Data }) => {
+  const data = props.data;
+  function toDegree(lv: number) {
+    if (0 < lv && lv < data.程度符号.length && data.程度符号[lv] !== false) {
+      return data.程度符号[lv];
+    }
+    return null;
+  }
+  const changeDegree = $(async (lv: number) => {
+    if (lv >= data.程度符号.length - 1) return 0;
+    while (data.程度符号[++lv] === false)
+      if (lv >= data.程度符号.length - 1) return 0;
+    return lv;
+  });
+  if (data.竞争者评价.length !== data.用户需求.length)
+    throw new Error("竞争者评价和用户需求必须匹配");
+  if (!data.竞争者评价?.length) throw new Error("用户需求数量必须大于 0");
+  if (data.竞争者评价[0].length !== data.技术手段.length)
+    throw new Error("竞争者评价和技术手段长度必须匹配");
+  if (data.技术相关性.length !== data.技术手段.length - 1)
+    throw new Error("竞争者评价和技术手段长度必须匹配");
+  data.技术相关性.forEach((ele, i) => {
+    if (ele?.length !== data.技术手段.length - 1 - i)
+      throw new Error("竞争者评价和技术手段长度必须呈递减匹配");
+  });
+
+  const 本企业技术指标值 = useComputed$(() => {
+    return data.技术手段.map((item, 技术) => {
+      let sum = 0;
+      for (let 需求 = 0; 需求 < data.用户需求.length; 需求++) {
+        const element = data.关系矩阵[需求][技术];
+        if (element !== undefined) sum += element * data.用户需求权重[需求];
+      }
+      return sum;
+    });
+  });
+
   return (
-    <table>
-      <tbody>
-        <tr class="header-row">
-          <td rowSpan={2}>用户需求</td>
-          <td rowSpan={2}>权重</td>
-          <td colSpan={4}>技术手段</td>
-          <td colSpan={5}>
-            竞争者评价（{props.竞争者评价分制.toLocaleString()}分制）
-          </td>
-        </tr>
-        <tr class="header-row">
-          {props.技术手段.map((item, i) => (
-            <td key={i}>{item}</td>
+    <div class="house">
+      <table class="f">
+        <tbody>
+          {data.技术相关性.map((item, i) => (
+            <tr key={"rooftop-" + i}>
+              {item.map((item, j) => (
+                <td key={"rooftop-" + i + "-" + j}>
+                  <button
+                    onClick$={async () =>
+                      (data.技术相关性[i][j] = await changeDegree(
+                        data.技术相关性[i][j]
+                      ))
+                    }
+                  >
+                    <div>{toDegree(item)}</div>
+                  </button>
+                </td>
+              ))}
+              <td class="disabled"></td>
+            </tr>
           ))}
-          {Array(props.竞争者评价分制)
-            .fill(0)
-            .map((_, i) => (
-              <td key={i}>{i + 1}</td>
+          <tr class="disabled">
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <tr class="header-row">
+            <td rowSpan={2} class="wide">
+              用户需求
+            </td>
+            <td rowSpan={2}>权重</td>
+            <td colSpan={data.技术手段.length}>技术手段</td>
+            <td colSpan={5}>
+              竞争者评价（{data.竞争者评价分制.toLocaleString("zh-CN")}分制）
+            </td>
+          </tr>
+          <tr class="header-row">
+            {data.技术手段.map((item, i) => (
+              <td class="技术手段" key={"tech-" + i}>
+                {item}
+              </td>
             ))}
-        </tr>
+            {Array(data.竞争者评价分制)
+              .fill(0)
+              .map((_, i) => (
+                <td class="竞争者评价" key={"竞争者评价-" + i}>
+                  {i + 1}
+                </td>
+              ))}
+          </tr>
+          {data.用户需求.map((item, i) => (
+            <tr key={"user-" + i} class="e">
+              <td>{item}</td>
+              <td>{data.用户需求权重[i]}</td>
+              {/* <td>xxx</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td> */}
+              {data.关系矩阵[i].map((item, j) => (
+                <td key={"E-" + i + "-" + j}>
+                  <button
+                    onClick$={async () =>
+                      (data.关系矩阵[i][j] = await changeDegree(
+                        data.关系矩阵[i][j]
+                      ))
+                    }
+                  >
+                    {toDegree(item)}
+                  </button>
+                </td>
+              ))}
+              {data.竞争者评价[i].map((item, j) => (
+                <Rater
+                  key={"rater-" + i + "-" + j}
+                  _key={i + "-" + j}
+                  data={data.竞争者评价[i][j]}
+                  handler={$((firm: string, check: boolean) => {
+                    // 确保当前行其余所有数据都为 false
+                    console.log(firm, check);
+                    if (check)
+                      for (let k = 0; k < data.竞争者评价[i].length; k++)
+                        if (k !== j) data.竞争者评价[i][k][firm] = false;
+                  })}
+                ></Rater>
+              ))}
+            </tr>
+          ))}
+          <tr>
+            <td colSpan={2}>本企业技术指标值</td>
+            {本企业技术指标值.value.map((item, i) => (
+              <td key={"index-" + i}>{item}</td>
+            ))}
+            <td rowSpan={7} colSpan={5} class="na"></td>
+          </tr>
+          <tr>
+            <td rowSpan={5}>技术评估（五分制）</td>
+            <td>5</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td> <td></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td></td>
+            <td></td>
+            <td></td> <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td> <td></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td></td>
+            <td></td>
+            <td></td> <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td></td>
+            <td></td>
+            <td></td> <td></td>
+            <td></td>
+          </tr>
 
-        <tr>
-          <td rowSpan={4}>技术手段</td>
-          <td>质量轻</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>方便使用</td>
-          <td></td>
-          <td></td> <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>容易握持</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td> <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>&gt;1500万像素</td>
-          <td></td>
-          <td></td>
-          <td></td> <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td colSpan={2}>本企业技术指标值</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td rowSpan={7} colSpan={5}></td>
-        </tr>
-        <tr>
-          <td rowSpan={5}>技术评估（五分制）</td>
-          <td>5</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-
-        <tr>
-          <td colSpan={2} class="title-row">
-            产品目标
-          </td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+          <tr>
+            <td colSpan={2} class="title-row">
+              产品目标
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 });
--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,54 @@
+import { component$, useStore } from "@builder.io/qwik";
 import { House } from "./components/house/house";
-import { Logo } from "./components/logo/logo";
 
-export default () => {
-  const defaultProps = {
-    技术手段: ["低电耗", "自动聚焦", "大容量电池", "外壳"],
+function toRateData(data: string[][]) {
+  return data.map((row) => {
+    return row.map((item) =>
+      item
+        .split("")
+        .filter(Boolean)
+        .reduce(
+          (p, c) => {
+            p[c] = true;
+            return p;
+          },
+          {
+            A: false,
+            B: false,
+            X: false,
+          } as { [key: string]: boolean }
+        )
+    );
+  });
+}
+
+const 竞争者评价 = toRateData([
+  ["B", "X", "", "A", ""],
+  ["XA", "", "", "B", ""],
+  ["", "", "A", "B", "X"],
+  ["", "X", "", "AB", ""],
+]);
+
+console.log(竞争者评价);
+
+export default component$(() => {
+  const data = useStore({
+    技术手段: ["低电耗", "自动聚焦", "大容量电池", "外壳", "折叠屏"],
+    技术相关性: [[1, 0, 0, 0], [0, 3, 0], [0, 5], [3]],
+    关系矩阵: [
+      [1, 1, 0, 0, 3],
+      [0, 5, 1, 0, 0],
+      [0, 0, 1, 1, 0],
+      [0, 0, 3, 1, 0],
+      [0, 0, 0, 0, 1],
+    ],
+    程度符号: [" ", "◎", false, "⊙", false, "▲"],
     用户需求: ["质量轻", "方便使用", "容易握持", "1500万像素"],
+    用户需求权重: [2, 3, 1, 4],
     竞争者评价分制: 5,
-  };
+    竞争者评价,
+  });
+
   return (
     <>
       <head>
@@ -14,9 +56,8 @@ export default () => {
         <title>Qwik Blank App</title>
       </head>
       <body>
-        <Logo />
-        <House {...defaultProps} />
+        <House data={data} />
       </body>
     </>
   );
-};
+});
